364a082212e10307463552204f07057ca8604547
Seth A Burleson <Sbrevolution5@aim.com>
2022-04-14 14:50:10 -0400

    initial setup and user model

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
@@ -3,12 +3,22 @@ package io.jzheaux.springsecurity.resolutions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class ResolutionsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ResolutionsApplication.class, args);
+	}
+	@Bean
+	public UserDetailsService userDetailsService(){
+		return new InMemoryUserDetailsManager(
+				org.springframework.security.core.userdetails.User.withUsername("user").password("{bcrypt}$2a$10$MywQEqdZFNIYnx.Ro/VQ0ulanQAl34B5xVjK2I/SDZNVGS5tHQ08W")
+						.authorities("resolution:read")
+						.build());
 	}
 
 }
--- src/main/java/io/jzheaux/springsecurity/resolutions/User.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/User.java
@@ -1,6 +1,61 @@
 package io.jzheaux.springsecurity.resolutions;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.io.Serializable;
+import java.util.UUID;
 
+@Entity(name="users")
 public class User implements Serializable {
+    User(){}
+    public User(String username, String password){
+        this.id = UUID.randomUUID();
+        this.username = username;
+        this.password = password;
+    }
+    @Id
+    private UUID id;
+
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+
+    @Column
+    private boolean enabled = true;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
 }
