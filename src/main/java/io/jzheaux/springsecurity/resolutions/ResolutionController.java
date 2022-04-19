55e895251bb299b2f2395a4d4ef9a285041ce51f
Seth A Burleson <Sbrevolution5@aim.com>
2022-04-19 11:44:56 -0400

    part 3 tasks 4

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionController.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionController.java
@@ -1,6 +1,7 @@
 package io.jzheaux.springsecurity.resolutions;
 
 import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ public class ResolutionController {
 
 	@GetMapping("/resolutions")
 	@PreAuthorize("hasAuthority('resolution:read')")
+	@PostFilter("filterObject.owner == authentication.name")
 	public Iterable<Resolution> read() {
 		return this.resolutions.findAll();
 	}
