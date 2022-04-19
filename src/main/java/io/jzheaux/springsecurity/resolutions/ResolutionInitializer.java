25062b59f1fd7820de511f49f77f2286577d61b7
Seth A Burleson <Sbrevolution5@aim.com>
2022-04-19 12:04:30 -0400

    automatically add read/write permission for admins

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionInitializer.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionInitializer.java
@@ -37,8 +37,6 @@ public class ResolutionInitializer implements SmartInitializingSingleton {
 
 		User admin = new User("admin","{bcrypt}$2a$10$bTu5ilpT4YILX8dOWM/05efJnoSlX4ElNnjhNopL9aPoRyUgvXAYa");
 		admin.grantAuthority("ROLE_ADMIN");
-		admin.grantAuthority("resolution:read");
-		admin.grantAuthority("resolution:write");
 		this.users.save(admin);
 	}
 }
--- src/main/java/io/jzheaux/springsecurity/resolutions/UserRepositoryUserDetailsService.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/UserRepositoryUserDetailsService.java
@@ -6,6 +6,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,19 +20,18 @@ public class UserRepositoryUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return this.users.findByUsername(username).map(BridgeUser::new)
+        return this.users.findByUsername(username).map(this::map)
                 .orElseThrow(()-> new UsernameNotFoundException("invalid user"));
     }
     private static class BridgeUser extends User implements UserDetails {
-        public BridgeUser(User user) {
+        private final Collection<GrantedAuthority> authorities;
+        public BridgeUser(User user, Collection<GrantedAuthority> authorities) {
             super(user);
+            this.authorities = authorities;
         }
 
-        public List<GrantedAuthority> getAuthorities() {
-            return this.getUserAuthorities().stream()
-                    .map(UserAuthority::getAuthority)
-                    .map(SimpleGrantedAuthority::new)
-                    .collect(Collectors.toList());
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            return this.authorities;
         }
 
         public boolean isAccountNonExpired() {
@@ -44,5 +45,19 @@ public class UserRepositoryUserDetailsService implements UserDetailsService {
         public boolean isCredentialsNonExpired() {
             return this.isEnabled();
         }
+
+    }
+    private BridgeUser map(User user){
+        Collection<GrantedAuthority> authorities = new HashSet<>();
+        for (UserAuthority userAuthority :
+                user.getUserAuthorities()) {
+            String authority = userAuthority.getAuthority();
+            if ("ROLE_ADMIN".equals(authority)){
+                authorities.add(new SimpleGrantedAuthority("resolution:read"));
+                authorities.add(new SimpleGrantedAuthority("resolution:write"));
+            }
+            authorities.add(new SimpleGrantedAuthority(authority));
+        }
+        return new BridgeUser(user, authorities);
     }
 }
