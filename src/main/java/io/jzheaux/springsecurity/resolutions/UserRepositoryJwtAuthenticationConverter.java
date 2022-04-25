60105ac99be29648fd5f77e52c4bba484e78fa53
Seth A Burleson <Sbrevolution5@aim.com>
2022-04-25 14:48:56 -0400

    customized user principal

--- src/main/java/io/jzheaux/springsecurity/resolutions/UserRepositoryJwtAuthenticationConverter.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/UserRepositoryJwtAuthenticationConverter.java
@@ -4,12 +4,16 @@ import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.BearerTokenAuthentication;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
+import java.util.Map;
 
 @Component
 public class UserRepositoryJwtAuthenticationConverter implements Converter<Jwt, AbstractAuthenticationToken> {
@@ -27,6 +31,31 @@ public class UserRepositoryJwtAuthenticationConverter implements Converter<Jwt, 
         User user = this.users.findByUsername(username)
                 .orElseThrow(()->new UsernameNotFoundException("no user"));
         Collection<GrantedAuthority> authorities = this.authoritiesConverter.convert(jwt);
-        return new JwtAuthenticationToken(jwt, authorities);
+        OAuth2AuthenticatedPrincipal principal = new UserOAuth2AuthenticatedPrincipal(user, jwt.getClaims(),authorities);
+        OAuth2AccessToken credentials = new OAuth2AccessToken(OAuth2AccessToken.TokenType.BEARER, jwt.getTokenValue(),null,null);
+        return new BearerTokenAuthentication(principal,credentials,authorities);
+    }
+    private  static class UserOAuth2AuthenticatedPrincipal extends User implements OAuth2AuthenticatedPrincipal{
+        private final Map<String,Object> attributes;
+        private final Collection<GrantedAuthority> authorities;
+        public UserOAuth2AuthenticatedPrincipal(User user, Map<String,Object> attributes, Collection<GrantedAuthority> authorities){
+            super(user);
+            this.attributes = attributes;
+            this.authorities = authorities;
+        }
+        @Override
+        public String getName(){
+            return this.getUsername();
+        }
+
+        @Override
+        public Map<String, Object> getAttributes() {
+            return this.attributes;
+        }
+
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            return this.authorities;
+        }
     }
 }
